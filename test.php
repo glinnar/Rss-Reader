2bc9a9561400e7a20055e19d2f2a315ba91c591b
Gurra <g.linnar@gmail.com>
2017-10-11 14:03:47 +0200

    update

--- test.php
+++ test.php
@@ -10,11 +10,14 @@ $xml_to_array= simplexml_load_file($url);
 
 $xml=json_decode(json_encode($xml_to_array),true);
 
-foreach ($xml as $read) {
-  echo $read;
-  # code...
+function my_sort($a, $b){
+    if ($a->pubDate-> == $b->pubDate) {
+        return 0;
+    }
+    return ($a->pubDate < $b->pubDate) ? -1 : 1;
 }
 
+usort($xml->item, "my_sort");
 
 
 /*$sort=array();
